9d806a883238c053842504378e2905d70886b313
addisonkline <77369109+addisonkline@users.noreply.github.com>
2026-03-05 16:25:01 -0500

    basic CLI client

--- src/mdrfc/client.py
+++ src/mdrfc/client.py
@@ -1,8 +1,10 @@
 from argparse import ArgumentParser, Namespace
 import json
+import os
 from urllib.parse import urlsplit
 
 import httpx
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import ValidationError
 from rich import print as rprint
 from rich.console import Console
@@ -11,11 +13,13 @@ from rich.text import Text
 from rich.style import Style
 
 import mdrfc.responses as res_types
+from mdrfc.backend.auth import Token, User
 from mdrfc.utils.version import get_mdrfc_version
 
 
 _url: str = None # type: ignore
 _username: str = "{unknown}"
+_token: str = None # type: ignore
 
 
 def _get_user_agent() -> str:
@@ -31,7 +35,8 @@ parser = ArgumentParser(
     usage="<command> [option]...",
     description="The MDRFC CLI client",
     epilog="Copyright (c) 2026 Addison Kline (GitHub: @addisonkline)",
-    add_help=False
+    add_help=False,
+    exit_on_error=False
 )
 subparsers = parser.add_subparsers(title="commands", dest="command")
 
@@ -39,8 +44,17 @@ subparsers = parser.add_subparsers(title="commands", dest="command")
 ping_desc = "Ping the remote server"
 ping_p = subparsers.add_parser(
     "ping",
+    usage="ping [option]...",
     help=ping_desc,
-    description=ping_desc
+    description=ping_desc,
+    add_help=False, # the default help option exits the REPL
+    exit_on_error=False,
+)
+ping_p.add_argument( # so replace it with one that doesn't
+    "-h",
+    "--help",
+    action="store_true",
+    help="show this help message and exit",
 )
 ping_p.add_argument(
     "-v",
@@ -49,11 +63,69 @@ ping_p.add_argument(
     help="include more detailed server info"
 )
 
+# log into the server
+login_desc = "Log into the remote server"
+login_p = subparsers.add_parser(
+    "login",
+    usage="login <username> <password> [option]...",
+    help=login_desc,
+    description=login_desc,
+    add_help=False,
+    exit_on_error=False
+)
+login_p.add_argument(
+    "username",
+    help="username for this MDRFC server"
+)
+login_p.add_argument(
+    "password",
+    help="associated password"
+)
+login_p.add_argument(
+    "-h",
+    "--help",
+    action="store_true",
+    help="show this help message and exit"
+)
+login_p.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="include more detailed login info"
+)
+
+# ask the server who this client is
+whoami_desc = "(login required) Get client info from this server"
+whoami_p = subparsers.add_parser(
+    "whoami",
+    usage="whoami [option]...",
+    help=whoami_desc,
+    description=whoami_desc,
+    add_help=False,
+    exit_on_error=False
+)
+whoami_p.add_argument(
+    "-h",
+    "--help",
+    action="store_true",
+    help="show this help message and exit"
+)
+whoami_p.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="include more detailed client info"
+)
+
 # command handlers
-def cmd_ping(args: Namespace) -> None:
+def _cmd_ping(args: Namespace) -> None:
     """
     Ping the remote MDRFC server.
     """
+    if args.help:
+        ping_p.print_help()
+        return
+
     global _url
     response = httpx.get(
         _url,
@@ -65,9 +137,8 @@ def cmd_ping(args: Namespace) -> None:
         return
     
     response_json = response.json()
-    response_json_str = json.dumps(response_json)
     try:
-        response_obj = res_types.GetRootResponse.model_validate_json(response_json_str)
+        response_obj = res_types.GetRootResponse.model_validate(response_json)
     except ValidationError as e:
         rprint("[bold red]error[/bold red] remote not recognized as an MDRFC server")
         return
@@ -80,6 +151,102 @@ def cmd_ping(args: Namespace) -> None:
         rprint(f"[bold]metadata[/bold]: {response_obj.metadata}")
     else:
         print("pong")
+
+
+def _cmd_login(args: Namespace) -> None:
+    """
+    Attempt to log into the server.
+    """
+    if args.help:
+        login_p.print_help()
+        return
+    
+    global _url
+    username = args.username
+    password = args.password
+
+    body = {
+        "grant_type": "password",
+        "username": username,
+        "password": password,
+        "scope": "",
+        "client_id": username,
+        "client_secret": password
+    }
+
+    response = httpx.post(
+        url=f"{_url}/login",
+        data=body,
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": _get_user_agent()
+        }
+    )
+
+    if response.status_code != 200:
+        rprint(f"[bold red]error[/bold red] request failed with status code [red]{response.status_code}[/red]")
+        return
+    
+    response_json = response.json()
+    try:
+        response_obj = Token.model_validate(response_json)
+    except ValidationError as e:
+        rprint("[bold red]error[/bold red] response validation failed")
+        rprint(e)
+        return
+
+    global _username
+    global _token
+    _username = username
+    _token = response_obj.access_token
+    if args.verbose:
+        rprint(f"[bold]token[/bold]: {response_obj.access_token}")
+        rprint(f"[bold]type[/bold]: {response_obj.token_type}")
+    else:
+        rprint(f"successfully logged in as [green]{_username}[/green]")
+
+
+def _cmd_whoami(args: Namespace) -> None:
+    """
+    Attempt to fetch client info from the server.
+    """
+    if args.help:
+        whoami_p.print_help()
+        return
+    
+    global _token
+    if _token is None:
+        rprint("[bold red]error[/bold red] not logged in")
+        return
+
+    global _url
+    response = httpx.get(
+        url=f"{_url}/users/me",
+        headers={
+            "Authorization": f"Bearer {_token}",
+            "User-Agent": _get_user_agent()
+        }
+    )
+
+    if response.status_code != 200:
+        rprint(f"[bold red]error[/bold red] request failed with status code [red]{response.status_code}[/red]")
+        return
+    
+    response_json = response.json()
+    try:
+        response_obj = User.model_validate(response_json)
+    except ValidationError as e:
+        rprint("[bold red]error[/bold red] response validation failed")
+        rprint(e)
+        return
+    
+    if args.verbose:
+        rprint(f"[bold]username[/bold]: {response_obj.username}")
+        rprint(f"[bold]email[/bold]: {response_obj.email}")
+        rprint(f"[bold]name[/bold]: {response_obj.name_last}, {response_obj.name_first}")
+        rprint(f"[bold]created[/bold]: {response_obj.created_at}")
+    else:
+        rprint(f"username: [green]{response_obj.username}[/green]")
 
 
 def _get_preamble() -> Text:
@@ -130,7 +297,9 @@ def _run_repl() -> None:
     console.print(_get_preamble())
 
     commands = {
-        "ping": cmd_ping
+        "ping": _cmd_ping,
+        "login": _cmd_login,
+        "whoami": _cmd_whoami,
     }
 
     running = True
@@ -148,13 +317,16 @@ def _run_repl() -> None:
         elif (user_input == "help") or (user_input == "?"):
             parser.print_help()
         else:
-            args = parser.parse_args(user_input.split())
-            handler = commands.get(args.command)
-            if handler:
-                handler(args)
-            else:
-                parser.print_usage()
-                print("for help, run `help`, `?`")
+            try:
+                args = parser.parse_args(user_input.split())
+                handler = commands.get(args.command)
+                if handler:
+                    handler(args)
+                else:
+                    parser.print_usage()
+                    print("for help, run `help`, `?`")
+            except Exception as e:
+                rprint(f"[bold red]error[/bold red] command failed: {e}")
 
 
 def _validate_url(url: str) -> None:
